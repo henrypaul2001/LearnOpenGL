5bac7da8f2b00d78193bb648a3a0b35f6ccd4f23
henrypaul2001 <henryp1.hp@googlemail.com>
2023-11-12 17:16:14 +0000

    Create simple vertex array for a triangle and buffered to GPU

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -38,6 +38,17 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	while (!glfwWindowShouldClose(window)) {
 		// input
 		processInput(window);
