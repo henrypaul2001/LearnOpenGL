5a14f84dabbbd4b4a5dc412b7cb6120da722bf5e
henrypaul2001 <henryp1.hp@googlemail.com>
2023-12-16 17:31:01 +0000

    Multiple cubes rendered with every 3rd cube rotating

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -199,8 +199,20 @@ int main() {
 	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 	// Model
-	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 model;
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3( 0.0f,   0.0f,    0.0f),
+		glm::vec3( 2.0f,   5.0f,  -15.0f),
+		glm::vec3(-1.5f,  -2.2f,   -2.5f),
+		glm::vec3(-3.8f,  -2.0f,  -12.3f),
+		glm::vec3( 2.4f,  -0.4f,   -3.5f),
+		glm::vec3(-1.7f,   3.0f,   -7.5f),
+		glm::vec3( 1.3f,  -2.0f,   -2.5f),
+		glm::vec3( 1.5f,   2.0f,   -2.5f),
+		glm::vec3( 1.5f,   0.2f,   -1.5f),
+		glm::vec3(-1.3f,   1.0f,   -1.5f)
+	};
 
 	double previousTime = glfwGetTime();
 	double currentTime = previousTime;
@@ -224,22 +236,25 @@ int main() {
 		// shader
 		defaultShader.use();
 
-		model = glm::rotate(model, 0.5f * (float)deltaTime, glm::vec3(1.0f, 0.0f, 0.0f));
 
-		unsigned int modelLocation = glGetUniformLocation(defaultShader.GetID(), "model");
-		glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
-
-		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.3f * deltaTime));
-
-		unsigned int viewLocation = glGetUniformLocation(defaultShader.GetID(), "view");
-		glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
-
-		unsigned int projectionLocation = glGetUniformLocation(defaultShader.GetID(), "projection");
-		glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		defaultShader.setMat4("view", view);
+		defaultShader.setMat4("projection", projection);
 
 		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		for (unsigned int i = 0; i < 10; i++) {
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+
+			float angle = 20.0f * i;
+			if (i % 3 == 0) {
+				angle *= glfwGetTime();
+			}
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+			defaultShader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// check and call events and swap buffers
 		glfwPollEvents();
