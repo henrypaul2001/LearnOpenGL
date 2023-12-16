c1ee039af701347ce351bb9f8aea1835c25c0bab
henrypaul2001 <henryp1.hp@googlemail.com>
2023-12-16 17:21:38 +0000

    Added set matrix function to shader class

--- LearnOpenGL/Shader.cpp
+++ LearnOpenGL/Shader.cpp
@@ -1,4 +1,5 @@
 #include "Shader.h"
+#include <glm/gtc/type_ptr.hpp>
 
 Shader::Shader(const char* vertexPath, const char* fragmentPath) {
 	// 1. retrieve source code from files
@@ -101,4 +102,9 @@ void Shader::setInt(const std::string& name, int value) const
 void Shader::setFloat(const std::string& name, float value) const
 {
 	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setMat4(const std::string& name, glm::mat4 value) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
