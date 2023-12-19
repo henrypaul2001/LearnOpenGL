c2835b5a700753208b6c86c464687d09b9558539
henrypaul2001 <henryp1.hp@googlemail.com>
2023-12-19 15:18:38 +0000

    Updated main class to set light properties in lighting shader

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -341,8 +341,6 @@ int main() {
 		// shader
 		//defaultShader.use();
 		lightingShader.use();
-		lightingShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
-		lightingShader.setVec3("lightPos", lightPos);
 		lightingShader.setVec3("viewPos", camera.Position);
 
 		// Material properties
@@ -350,6 +348,12 @@ int main() {
 		lightingShader.setVec3("material.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
 		lightingShader.setVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
 		lightingShader.setFloat("material.shininess", 32.0f);
+
+		// Light properties
+		lightingShader.setVec3("light.position", lightPos);
+		lightingShader.setVec3("light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
+		lightingShader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+		lightingShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
 
 		// Update camera
 		projection = glm::perspective(glm::radians(camera.Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
