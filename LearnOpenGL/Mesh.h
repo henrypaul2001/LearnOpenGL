046b12fd69c93405d60bbea5848aef9c601810f2
henrypaul2001 <henryp1.hp@googlemail.com>
2023-12-21 18:48:04 +0000

    Implemented loadMaterialTextures method and added helper function

--- LearnOpenGL/Mesh.h
+++ LearnOpenGL/Mesh.h
@@ -13,6 +13,7 @@ struct Vertex {
 struct Texture {
 	unsigned int id;
 	std::string type;
+	std::string filepath;
 };
 
 class Mesh
--- LearnOpenGL/Model.h
+++ LearnOpenGL/Model.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Shader.h"
 #include "Mesh.h"
+#include "stb_image.h"
 
 #include <vector>
 #include <string>
@@ -19,9 +20,11 @@ public:
 private:
 	std::vector<Mesh> meshes;
 	std::string directory;
+	std::vector<Texture> textures_loaded;
 
 	void loadModel(std::string filepath);
 	void processNode(aiNode* node, const aiScene* scene);
 	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
 	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+	unsigned int TextureFromFile(const char* path, const std::string& directory);
 };
