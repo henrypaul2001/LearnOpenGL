93855fa30bffff0825b7e05df57a3e2730981591
henrypaul2001 <henryp1.hp@googlemail.com>
2023-12-21 17:26:10 +0000

    Implemented mesh class constructor

--- LearnOpenGL/Mesh.cpp
+++ LearnOpenGL/Mesh.cpp
@@ -1,1 +1,18 @@
 #include "Mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	setupMesh();
+}
+
+void Mesh::Draw(Shader& shader)
+{
+}
+
+void Mesh::setupMesh()
+{
+}
