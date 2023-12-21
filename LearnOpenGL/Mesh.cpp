c9b47c891d0285a9e1b82e9601a8bd1554370801
henrypaul2001 <henryp1.hp@googlemail.com>
2023-12-21 17:44:40 +0000

    Implemented draw method in mesh class

--- LearnOpenGL/Mesh.cpp
+++ LearnOpenGL/Mesh.cpp
@@ -11,6 +11,28 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std:
 
 void Mesh::Draw(Shader& shader)
 {
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	for (unsigned int i = 0; i < textures.size(); i++) {
+		glActiveTexture(GL_TEXTURE0 + i);
+		std::string number;
+		std::string name = textures[i].type;
+		if (name == "texture_diffuse") {
+			number = std::to_string(diffuseNr++);
+		}
+		else if (name == "texture_specular") {
+			number = std::to_string(specularNr++);
+		}
+
+		shader.setFloat(("material." + name + number).c_str(), i);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+	glActiveTexture(GL_TEXTURE0);
+
+	// draw
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 void Mesh::setupMesh()
