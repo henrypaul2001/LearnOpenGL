e35ccbfa7884349680460e5eb45e448bf18102fd
henrypaul2001 <henryp1.hp@googlemail.com>
2024-01-07 15:48:55 +0000

    Added custom framebuffer object

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -491,6 +491,39 @@ int runScene2() {
 	glEnable(GL_CULL_FACE);
 	glFrontFace(GL_CW);
 
+	unsigned int framebuffer;
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+	// framebuffer texture
+	unsigned int texColourBuffer;
+	glGenTextures(1, &texColourBuffer);
+	glBindTexture(GL_TEXTURE_2D, texColourBuffer);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// attach texture to framebuffer
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColourBuffer, 0);
+
+	// set up render buffer object for depth and stencil testing (render buffer = fast but no sampling)
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	// attach depth and stencil buffer to framebuffer object
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+	// framebuffer error checking
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 	// build and compile shaders
 	// -------------------------
 	Shader shader("depth_testing.vert", "depth_testing.frag");
@@ -562,6 +595,16 @@ int runScene2() {
 		0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
 		1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
 		1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+	};
+	float quadVertices[] = {
+		// positions    // texCoords
+		-1.0f, 1.0f,    0.0f, 1.0f,
+		-1.0f, -1.0f,   0.0f, 0.0f,
+		1.0f, -1.0f,    1.0f, 0.0f,
+
+		-1.0f, 1.0f,    0.0f, 1.0f,
+		1.0f, -1.0f,    1.0f, 0.0f,
+		1.0f, 1.0f,     1.0f, 1.0f
 	};
 	// cube VAO
 	unsigned int cubeVAO, cubeVBO;
