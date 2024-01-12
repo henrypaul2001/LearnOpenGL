3d10bd6549dcd8e53ae7d0c0c83b8ee66ffcc4fb
henrypaul2001 <henryp1.hp@googlemail.com>
2024-01-12 14:20:05 +0000

    Updated shader class with setVec2 method

--- LearnOpenGL/Shader.h
+++ LearnOpenGL/Shader.h
@@ -23,6 +23,8 @@ public:
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
 	void setMat4(const std::string& name, glm::mat4 value) const;
+	void setVec2(const std::string& name, glm::vec2 value) const;
+	void setVec2(const std::string& name, float x, float y) const;
 	void setVec3(const std::string& name, glm::vec3 value) const;
 	void setVec3(const std::string& name, float x, float y, float z) const;
 	unsigned int GetID() { return ID; }
