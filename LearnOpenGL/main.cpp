4017184af882900e383e002f894896e0b6e6b4a7
henrypaul2001 <henryp1.hp@googlemail.com>
2024-01-14 14:51:27 +0000

    Enabled MSAA

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -474,6 +474,7 @@ int runScene2() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 
 #ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -516,6 +517,8 @@ int runScene2() {
 
 	glEnable(GL_CULL_FACE);
 	glFrontFace(GL_CW);
+
+	glEnable(GL_MULTISAMPLE);
 
 	unsigned int framebuffer;
 	glGenFramebuffers(1, &framebuffer);
