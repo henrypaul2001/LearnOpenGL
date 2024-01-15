692ec5ffeeaa71c22bdf208e71907bfea4f6a0e2
henrypaul2001 <henryp1.hp@googlemail.com>
2024-01-15 15:24:02 +0000

    Enabled srgb framebuffer gamma correction

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1210,6 +1210,7 @@ int runScene3() {
 	// -----------------------------
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
+	glEnable(GL_FRAMEBUFFER_SRGB);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// build and compile shaders
