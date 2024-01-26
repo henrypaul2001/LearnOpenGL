8131a6b4a55f553f9e3a4955f123e2e5a76dbcde
henrypaul2001 <henryp1.hp@googlemail.com>
2024-01-26 16:19:41 +0000

    Set up SSAO kernel sample generation

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -10,6 +10,7 @@
 #include "Camera.h"
 #include "Model.h"
 #include <map>
+#include <random>
 
 static float deltaTime = 0.0f;
 
@@ -3239,6 +3240,20 @@ int runScene9() {
 
 	// generate sample kernel
 	// ----------------------
+	std::uniform_real_distribution<GLfloat> randomFloats(0.0, 1.0); // generates random floats between 0.0 and 1.0
+	std::default_random_engine generator;
+	std::vector<glm::vec3> ssaoKernel;
+	for (unsigned int i = 0; i < 64; i++) {
+		glm::vec3 sample(randomFloats(generator) * 2.0 - 1.0, randomFloats(generator) * 2.0 - 1.0, randomFloats(generator));
+		sample = glm::normalize(sample);
+		sample *= randomFloats(generator);
+		float scale = float(i) / 64.0f;
+
+		// scale samples to be more aligned with center of kernel
+		scale = lerp(0.1f, 1.0f, scale * scale);
+		sample *= scale;
+		ssaoKernel.push_back(sample);
+	}
 
 	// generate noise texture
 	// ----------------------
