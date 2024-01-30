2b1fe7a5a316e4b5b27f5701c4271e6fa6c1d624
henrypaul2001 <henryp1.hp@googlemail.com>
2024-01-30 17:15:02 +0000

    Added setMat3 function to shader class

--- LearnOpenGL/Shader.cpp
+++ LearnOpenGL/Shader.cpp
@@ -213,6 +213,11 @@ void Shader::setMat4(const std::string& name, glm::mat4 value) const
 	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
 
+void Shader::setMat3(const std::string& name, glm::mat3 value) const
+{
+	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
+
 void Shader::setVec2(const std::string& name, glm::vec2 value) const
 {
 	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
--- LearnOpenGL/Shader.h
+++ LearnOpenGL/Shader.h
@@ -23,6 +23,7 @@ public:
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
 	void setMat4(const std::string& name, glm::mat4 value) const;
+	void setMat3(const std::string& name, glm::mat3 value) const;
 	void setVec2(const std::string& name, glm::vec2 value) const;
 	void setVec2(const std::string& name, float x, float y) const;
 	void setVec3(const std::string& name, glm::vec3 value) const;
