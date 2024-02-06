47657d22d4784aa2fede4a0c9e56c26ad0b5ea3c
henrypaul2001 <henryp1.hp@googlemail.com>
2024-02-06 16:24:03 +0000

    Added text rendering

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -12,7 +12,7 @@
 #include <map>
 #include <random>
 
-#include "ft2build.h"
+#include <ft2build.h>
 #include FT_FREETYPE_H
 
 static float deltaTime = 0.0f;
@@ -69,6 +69,15 @@ const unsigned int SCR_HEIGHT = 1080;
 const unsigned int MSAASamples = 4;
 const bool gammaCorrection = false;
 const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+struct Character {
+	unsigned int TextureID; // ID of glyph texture
+	glm::ivec2 Size;	    // size of glyph
+	glm::ivec2 Bearing;     // offset from baseline to left/top of glyph
+	unsigned int Advance;   // offset to next glyph
+};
+
+std::map<char, Character> Characters;
 
 float lerp(float a, float b, float f)
 {
@@ -691,6 +700,51 @@ void DisplayFramebufferTexture(unsigned int textureID, Shader& fboDebug) {
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	renderQuad();
+}
+
+void RenderText(Shader& s, std::string text, float x, float y, float scale, glm::vec3 color, unsigned int& textVAO, unsigned int& textVBO) {
+	s.use();
+	s.setVec3("textColor", color);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(textVAO);
+
+	// iterate through all characters in input
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++) {
+		Character ch = Characters[*c];
+
+		float xpos = x + ch.Bearing.x * scale;
+		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		float w = ch.Size.x * scale;
+		float h = ch.Size.y * scale;
+
+		// update VBO for each character
+		float vertices[6][4] = {
+			{ xpos, ypos + h, 0.0f, 0.0f },
+			{ xpos, ypos, 0.0f, 1.0f },
+			{ xpos + w, ypos, 1.0f, 1.0f },
+			{ xpos, ypos + h, 0.0f, 0.0f },
+			{ xpos + w, ypos, 1.0f, 1.0f },
+			{ xpos + w, ypos + h, 1.0f, 0.0f }
+		};
+
+		// render glyph over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+
+		// update VBO
+		glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		// render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// advance cursors for next glyph
+		x += (ch.Advance >> 6) * scale;
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 int runScene1() {
@@ -3845,6 +3899,10 @@ int runScene11() {
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND); // important for text rendering
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// glfwSwapInterval(0); // disable v-sync
 
 	// Framebuffers
 	// ------------
@@ -3880,6 +3938,7 @@ int runScene11() {
 	Shader prefilterShader("convert_to_cubemap.vert", "prefilter.frag");
 	Shader brdfShader("brdf.vert", "brdf.frag");
 	Shader fboDebug("debugFramebufferView.vert", "debugFramebufferView.frag");
+	Shader textShader("text.vert", "text.frag");
 
 	// initialize static shader uniforms before rendering
 	// --------------------------------------------------
@@ -3903,6 +3962,9 @@ int runScene11() {
 
 	skyboxShader.use();
 	skyboxShader.setInt("environmentMap", 0);
+
+	textShader.use();
+	textShader.setInt("text", 0);
 
 	// Load textures
 	// -------------
@@ -4082,10 +4144,65 @@ int runScene11() {
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 	glEnable(GL_CULL_FACE);
 
+	// Font loading
+	// ------------
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft)) {
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+	}
+
+	FT_Face face;
+	if (FT_New_Face(ft, "Fonts/arial.ttf", 0, &face)) {
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+	}
+
+	FT_Set_Pixel_Sizes(face, 0, 48);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // no byte-alignment restriction
+	for (unsigned char c = 0; c < 128; c++) {
+		// load character glyph
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+			std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+
+		// generate texture
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(std::pair<char, Character>(c, character));
+	}
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	glm::mat4 textProjection = glm::ortho(0.0f, (float)SCR_WIDTH, 0.0f, (float)SCR_HEIGHT);
+
+	unsigned int textVAO, textVBO;
+	glGenVertexArrays(1, &textVAO);
+	glGenBuffers(1, &textVBO);
+	glBindVertexArray(textVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 	// Model loading
 	// -------------
 	Model brass_goblets = Model("Models/brass_goblet/brass_goblet.obj");
-	Model cart = Model("Models/cart/cart.obj");
+	//Model cart = Model("Models/cart/cart.obj");
 
 	// render loop
 	// -----------
@@ -4189,7 +4306,7 @@ int runScene11() {
 		model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
 		shader.setMat4("model", model);
 		shader.setMat3("normalMatrix", glm::transpose(glm::inverse(glm::mat3(model))));
-		cart.Draw(shader);
+		//cart.Draw(shader);
 
 		// render skybox
 		glDisable(GL_CULL_FACE);
@@ -4202,7 +4319,11 @@ int runScene11() {
 		glEnable(GL_CULL_FACE);
 
 		// debug fbo
-		DisplayFramebufferTexture(brdfLUTTexture, fboDebug);
+		//DisplayFramebufferTexture(brdfLUTTexture, fboDebug);
+
+		textShader.use();
+		textShader.setMat4("projection", textProjection);
+		RenderText(textShader, "sample text / sample text / sample text", 25.0f, 25.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f), textVAO, textVBO);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
