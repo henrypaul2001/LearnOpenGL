c87c2e971630e15084a400ed4b8aac64d13cf497
henrypaul2001 <henryp1.hp@googlemail.com>
2024-09-12 14:51:33 +0100

    Added uniform utility functions to compute shader class

--- LearnOpenGL/ComputeShader.cpp
+++ LearnOpenGL/ComputeShader.cpp
@@ -1,4 +1,5 @@
 #include "ComputeShader.h"
+#include <glm/gtc/type_ptr.hpp>
 
 ComputeShader::ComputeShader(const char* computePath)
 {
@@ -63,4 +64,64 @@ ComputeShader::ComputeShader(const char* computePath)
 
 ComputeShader::~ComputeShader()
 {
+}
+
+void ComputeShader::use() const
+{
+	glUseProgram(ID);
+}
+
+void ComputeShader::setBool(const std::string& name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void ComputeShader::setInt(const std::string& name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void ComputeShader::setFloat(const std::string& name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void ComputeShader::setMat4(const std::string& name, glm::mat4 value) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void ComputeShader::setMat3(const std::string& name, glm::mat3 value) const
+{
+	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void ComputeShader::setVec2(const std::string& name, glm::vec2 value) const
+{
+	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void ComputeShader::setVec2(const std::string& name, float x, float y) const
+{
+	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+}
+
+void ComputeShader::setVec3(const std::string& name, glm::vec3 value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void ComputeShader::setVec3(const std::string& name, float x, float y, float z) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
+void ComputeShader::setVec4(const std::string& name, glm::vec4 value) const
+{
+	glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void ComputeShader::setVec4(const std::string& name, float x, float y, float z, float w) const
+{
+	glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
 }
--- LearnOpenGL/ComputeShader.h
+++ LearnOpenGL/ComputeShader.h
@@ -12,7 +12,21 @@ public:
 	ComputeShader(const char* computePath);
 	~ComputeShader();
 
+	void use() const;
 	const unsigned int GetID() const { return ID; }
+
+	// uniform utility functions
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+	void setMat4(const std::string& name, glm::mat4 value) const;
+	void setMat3(const std::string& name, glm::mat3 value) const;
+	void setVec2(const std::string& name, glm::vec2 value) const;
+	void setVec2(const std::string& name, float x, float y) const;
+	void setVec3(const std::string& name, glm::vec3 value) const;
+	void setVec3(const std::string& name, float x, float y, float z) const;
+	void setVec4(const std::string& name, glm::vec4 value) const;
+	void setVec4(const std::string& name, float x, float y, float z, float w) const;
 private:
 	unsigned int ID;
 };
